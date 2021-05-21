a8ba7cb12e9a384adcd47efe9c9afe54d440c79c
Yiqin <zhangyiqin217@gmail.com>
2021-05-21 01:23:37 -0400

    update db

---
+++ src/main/java/com/laioffer/job/servlet/ExampleJobServlet.java
@@ -0,0 +1,30 @@
+package com.laioffer.job.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.laioffer.job.entity.ExampleCoordinates;
+import com.laioffer.job.entity.ExampleJob;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ExampleJobServlet", value = "/ExampleJobServlet")
+public class ExampleJobServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        ObjectMapper mapper = new ObjectMapper();
+//        ExampleCoordinates coordinates = new ExampleCoordinates(37.485130, -122.148316);
+        ExampleCoordinates coordinates = new ExampleCoordinates(47.6062, -122.3321);
+        ExampleJob job = new ExampleJob("Software Engineer", 123456, "Aug 1, 2020", false, coordinates);
+        response.getWriter().print(mapper.writeValueAsString(job));
+    }
+
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
